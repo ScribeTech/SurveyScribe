957cf075b338ea1fad6f859e2a9b0bd56d32b538
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-03 15:29:34 -0500

    cant get this link to work

--- client/components/LandingPage/LandingPage.jsx
+++ client/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router';
 
-const LandingPage = () => <p>LandingPage</p>;
+console.log('link', Link);
+const LandingPage = () => (
+  <div>
+    <p>LandingPage</p>
+    <Link to="/" >Survy Test link</Link>
+  </div>
+
+);
 
 export default LandingPage;
