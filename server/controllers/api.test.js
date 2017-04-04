e0ba89e6976554c5f20d19df26289036622c3ae7
Elijah Schow <elijah.schow@gmail.com>
2017-04-04 09:07:50 -0500

    Refactor test

--- server/controllers/api.test.js
+++ server/controllers/api.test.js
@@ -113,17 +113,17 @@ module.exports = () => {
   describe('/PUT/:id survey', () => {
     it('PUTs a survey with the given id', (done) => {
       // Add example data before making a request
-      const initial = { title: 'Example Survey', questions: [{ label: 'What is your favorite color?', options: [{ label: 'Red', value: 0 }, { label: 'Green', value: 0 }, { label: 'Blue', value: 0 }] }, { label: 'Which do you like more?', options: [{ label: 'Dogs', value: 0 }, { label: 'Cats', value: 0 }] }] };
-      const final = { title: 'Example Survey', questions: [{ label: 'Do you weigh more than a duck?', options: [{ label: 'Yes', value: 0 }, { label: 'No', value: 0 }] }] };
+      const seed = { title: 'Example Survey', questions: [{ label: 'What is your favorite color?', options: [{ label: 'Red', value: 0 }, { label: 'Green', value: 0 }, { label: 'Blue', value: 0 }] }, { label: 'Which do you like more?', options: [{ label: 'Dogs', value: 0 }, { label: 'Cats', value: 0 }] }] };
+      const expected = { title: 'Example Survey', questions: [{ label: 'Do you weigh more than a duck?', options: [{ label: 'Yes', value: 0 }, { label: 'No', value: 0 }] }] };
       let id; // TODO: use promise.join or similar from bluebird
-      Survey.create(initial)
+      Survey.create(seed)
       .then((result) => {
         id = result._id;
-        return request(app).put(`/api/surveys/${id}`).send(final);
+        return request(app).put(`/api/surveys/${id}`).send(expected);
       })
       .then(result => request(app).get(`/api/surveys/${id}`))
       .then((response) => {
-        expect(response.body).to.shallowDeepEqual(final);
+        expect(response.body).to.shallowDeepEqual(expected);
         done();
       })
       .catch(done); // call "done" if the promise is rejected (see error.js)
