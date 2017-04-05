47850d1ebfbc1b80d205dad057eb251cfae622c8
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-05 14:41:53 -0500

    changing back to es6 class

--- client/components/SurveyGrid.jsx
+++ client/components/SurveyGrid.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import SurveySingle from './SurveySingle.jsx';
 
-const Survey = React.createClass({
 
-  componentDidMount() {
+const Survey = (props) => {
+
+  const concomponentDidMount = () => {
     console.log("props", this.props.surveys);
-  },
+  };
 
-  render() {
-    return (
-      <div>
-        <p>Survey</p>
-        <div className="SurveyList">
-          {this.props.surveys.map((survey) => {
-            return <SurveySingle surveysingle={survey} />;
-          })}
-        </div>
+  return (
+    <div>
+      <p>Survey</p>
+      <div className="SurveyList">
+        {this.props.surveys.map((survey) => {
+          return <SurveySingle surveysingle={survey} />;
+        })}
       </div>
-    );
-  }
-});
+    </div>
+  );
+};
 
 export default Survey;
