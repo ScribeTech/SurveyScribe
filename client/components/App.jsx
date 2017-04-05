145dec8403fe4b7402b0fa6060439d79a88dc064
Won Jin Chung <chungw51993@gmail.com>
2017-04-05 12:17:18 -0500

    Fixed merge conflict

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import io from 'socket.io-client';
 import * as actionCreators from '../actions/actionCreators';
 import Main from './Main';
 
+
 const clientSocket = io('http://localhost:8080', { path: '/api/result' });
 
 function mapStateToProps(state) {
