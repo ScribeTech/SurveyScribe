b28740c0519e7041133a74d962406ef315c2fb57
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-04 19:30:36 -0500

    got github cat included

--- client/components/LandingPage/LandingPage.jsx
+++ client/components/LandingPage/LandingPage.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Center from 'react-center';
 import { Link } from 'react-router';
 import RaisedButton from 'material-ui/RaisedButton';
-import FontIcon from 'material-ui/FontIcon';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Logo from './../../styles/Logo.png';
+import Github from './../../styles/GitHub-Mark-32px.png';
 import css from './../../styles/stylesheet.css';
 
 const LandingPage = () => (
@@ -12,9 +12,8 @@ const LandingPage = () => (
     <MuiThemeProvider>
       <RaisedButton
         href="https://github.com/ScribeTech/SurveyScribe"
-        target="_blank"
         label="Github Link"
-        icon={<FontIcon className="muidocs-icon-custom-github" />}
+        icon={<img className="github" src={Github} alt="github" />}
       />
     </MuiThemeProvider>
     <Center >
