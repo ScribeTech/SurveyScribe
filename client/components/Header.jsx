199d985299e32031f44635fe6b6eefb727522d31
Elijah Schow <elijah.schow@gmail.com>
2017-04-06 16:56:02 -0500

    Separate header into its own component

---
+++ client/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
+import { hashHistory as history } from 'react-router';
+
+const Header = props => (
+  <AppBar
+    title={props.title}
+    onTitleTouchTap={props.onTitleTouchTap}
+    iconElementLeft={
+      <IconButton onClick={() => history.goBack()}>
+        <NavigationArrowBack color="white" />
+      </IconButton>
+    }
+    iconElementRight={props.actions &&
+      <IconMenu iconButtonElement={<IconButton touch><MoreVertIcon /></IconButton>}>
+        {props.actions.map(p => (
+          <MenuItem primaryText={p.label} onClick={p.callback} />
+        ))}
+      </IconMenu>
+    }
+  />
+);
+
+Header.propTypes = {
+  onTitleTouchTap: React.PropTypes.func,
+  title: React.PropTypes.string,
+  actions: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      label: React.PropTypes.string.isRequired,
+      callback: React.PropTypes.func.isRequired
+    })
+  )
+};
+
+Header.defaultProps = {
+  onTitleTouchTap: () => { /* no-op */ },
+  title: 'Survey Scribe',
+  actions: null
+};
+
+export default Header;
--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -1,37 +1,17 @@
 import React from 'react';
-
-import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import NavigationArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
-import { hashHistory as history } from 'react-router';
 
+import Header from './Header';
 import '../assets/normalize.css';
 import '../assets/stylesheet.css';
 
 const Layout = props => (
   <MuiThemeProvider>
     <div>
-      <AppBar
-        title={props.title}
-        onTitleTouchTap={props.onTitleTouchTap}
-        iconElementLeft={
-          <IconButton onClick={() => history.goBack()}>
-            <NavigationArrowBack color="white" />
-          </IconButton>
-        }
-        iconElementRight={props.actions &&
-          <IconMenu iconButtonElement={<IconButton touch><MoreVertIcon /></IconButton>}>
-            {props.actions.map(p => (
-              <MenuItem primaryText={p.label} onClick={p.callback} />
-            ))}
-          </IconMenu>
-        }
-      />
-      <div className="content">{props.children}</div>
+      <Header {...props} />
+      <div className="content">
+        {props.children}
+      </div>
     </div>
   </MuiThemeProvider>
 );
@@ -41,20 +21,6 @@ Layout.propTypes = {
     React.PropTypes.element,
     React.PropTypes.arrayOf(React.PropTypes.element)
   ]).isRequired,
-  onTitleTouchTap: React.PropTypes.func,
-  title: React.PropTypes.string,
-  actions: React.PropTypes.arrayOf(
-    React.PropTypes.shape({
-      label: React.PropTypes.string.isRequired,
-      callback: React.PropTypes.func.isRequired
-    })
-  )
-};
-
-Layout.defaultProps = {
-  onTitleTouchTap: () => { /* no-op */ },
-  title: 'Survey Scribe',
-  actions: null
 };
 
 export default Layout;
