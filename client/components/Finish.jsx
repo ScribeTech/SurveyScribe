ca1607c1434af1a58c9017580c6bc8a9e758bb50
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-06 17:32:43 -0500

    working on finish view

--- client/components/Finish.jsx
+++ client/components/Finish.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Layout from './Layout';
 
-const Finish = props => <p>Finish</p>;
+const Finish = props => (
+  <Layout title="Survey Finished">
+    <p>Finish</p>
+  </Layout>
+);
 
 export default Finish;
