0b61a1ff3bb9b9e7563547855d9bd9d90be3494c
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-06 15:47:20 -0500

    added delete button

--- client/components/SurveyEditSingle.jsx
+++ client/components/SurveyEditSingle.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import Trash from './../Styles/TrashCan.png';
 
 const EditSingle = (props) => {
   const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -11,6 +12,9 @@ const EditSingle = (props) => {
     },
     option: {
       marginLeft: '50px'
+    },
+    trash: {
+      marginLeft: '20px'
     }
   };
 
@@ -27,6 +31,9 @@ const EditSingle = (props) => {
             id={props.survey.id.toString()}
             defaultValue={question.label}
           />
+          <span>
+            <img style={styles.trash} src={Trash} alt="trash" />
+          </span>
           {props.options[0][question.id].map((option, j) => (
             <div style={styles.option}>
               <span>{alphabet[j] + '.   '}</span>
@@ -34,10 +41,12 @@ const EditSingle = (props) => {
                 id={props.survey.id.toString()}
                 defaultValue={option}
               />
+              <span>
+                <img style={styles.trash} src={Trash} alt="trash" />
+              </span>
             </div>
           ))}
           <RaisedButton label="Add Choice" style={styles.option} />
-
         </div>
       ))}
     </div>
