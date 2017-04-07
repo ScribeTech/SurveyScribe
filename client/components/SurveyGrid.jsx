5c7904ab3b3527061a73d1d06ff4ffc049ad85a5
Won Jin Chung <chungw51993@gmail.com>
2017-04-07 11:26:03 -0500

    Working on creating new survey

--- client/components/SurveyGrid.jsx
+++ client/components/SurveyGrid.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { Card, CardTitle, CardActions } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import axios from 'axios';
 import Layout from './Layout';
 
 const styles = {
@@ -35,6 +38,33 @@ const actions = [
   { label: 'Delete', callback: () => {} }
 ];
 
+const handleClick = () => {
+  axios.post('http://localhost:8080/api/surveys', {
+    title: 'New Survey',
+    questions: [
+      {
+        _id: '0',
+        label: '',
+        options: [
+          {
+            label: '',
+            votes: 0
+          }
+        ]
+      }
+    ]
+  },
+    {
+      headers: { 'X-Requested-With': 'XMLHttpRequest' }
+    })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    throw error;
+  });
+};
+
 const SurveyGrid = props => (
   <Layout title="Surveys" actions={actions}>
     <Grid>
@@ -42,6 +72,9 @@ const SurveyGrid = props => (
         {props.surveys.map(survey => <SurveyTile key={survey.id} {...survey} />)}
       </Row>
     </Grid>
+    <FloatingActionButton className="floatingActionButton" onClick={handleClick}>
+      <ContentAdd />
+    </FloatingActionButton>
   </Layout>
 );
 
