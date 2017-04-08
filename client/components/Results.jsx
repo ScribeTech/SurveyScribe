0f9f53f14a2a0f38c221f5edb08fba93151507af
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-08 15:02:26 -0500

    got table printing with data

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -1,28 +1,68 @@
 import React from 'react';
 import Layout from './Layout';
 import ReactHighcharts from 'react-highcharts';
-import { render } from 'react-dom';
 
-const config = {
-  xAxis: {
-    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-  },
-  series: [{
-    data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
-  }]
+
+const Results = (props) => {
+  const surveyID = props.params.surveyID;
+  //const questions = [];
+  // const votes = [];
+  // props.questions[surveyID].forEach((question) => {
+  //   questions.push(question.label);
+  //   props.options[question.id].forEach(option => votes.push(option.votes));
+  // });
+  // console.log("votes", votes)
+  // const config = {
+  //   chart: {
+  //     type: 'bar'
+  //   },
+  //   xAxis: {
+  //     categories: questions
+  //   },
+  //   series: [{
+  //     data: votes
+  //   }]
+  // };
+
+  const makeConfig = (question) => {
+    const votes = [];
+    const options = [];
+    props.options[question.id].forEach((option) => {
+      votes.push(option.votes);
+      options.push(option.label);
+    });
+    const config = {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: question.label
+      },
+      xAxis: {
+        categories: options
+      },
+      series: [{
+        data: votes
+      }]
+    };
+
+    return config;
+  };
+
+  return (
+    <Layout title="Results">
+      <h1>{props.surveys[props.params.surveyID - 1].title}</h1>
+      <div>
+        {props.questions[surveyID].map((question) => {
+          return <ReactHighcharts config={makeConfig(question)} />
+        })}
+        test
+      </div>
+    </Layout>
+  );
 };
 
-
-const Results = props => (
-  <Layout title="Results">
-    <h1>{props.surveys[props.params.surveyID].title}</h1>
-    <ReactHighcharts config={config} />
-  </Layout>
-);
-
 Results.propTypes = {}.isRequired;
-
-//render(React.createElement(ReactHighcharts, { config }), document.getElementById('graph'));
 
 export default Results;
 
