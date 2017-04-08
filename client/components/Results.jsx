c18fb0557fd56073aed2ed56a1420bf9289c3d55
Elijah Schow <elijah.schow@gmail.com>
2017-04-08 11:20:06 -0500

    Bug/results - Go to Survey Results (#95)
    * Remove files that shouldn't have been tracked
    
    * (fix) Results displays the wrong survey title - closes #88
    
    * (doc) Update comments

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Layout from './Layout';
 
-const Results = props => (
-  <Layout title="Results">
-    <h1>{props.surveys[props.params.surveyID].title}</h1>
-  </Layout>
-);
+const Results = (props) => {
+  // Load the currently selected survey
+  // TODO: move this code to middleware (see issue #94)
+  const surveyID = props.params.surveyID;
+  const [survey] = props.surveys.filter(s => s.id === surveyID);
+  // Render
+  return (
+    <Layout title="Results">
+      <h1>{survey.title}</h1>
+    </Layout>
+  );
+};
 
 Results.propTypes = {}.isRequired;
 
--- client/components/SurveyGrid.jsx
+++ client/components/SurveyGrid.jsx
@@ -15,27 +15,6 @@ const styles = {
   }
 };
 
-const SurveyTile = props => (
-  <Col xs={12} sm={6} md={4}>
-    <Card style={styles.card}>
-      <CardTitle title={props.title} />
-      <CardActions>
-        <Link to={`survey/${props.id}/edit`}><FlatButton label="Edit" /></Link>
-        <Link to={`survey/${props.id}/results`}><FlatButton label="Results" /></Link>
-        <Link to={`survey/${props.id}/answer`}><FlatButton label="Share" /></Link>
-      </CardActions>
-    </Card>
-  </Col>
-);
-
-SurveyTile.propTypes = {
-  title: React.PropTypes.string.isRequired,
-  id: React.PropTypes.oneOfType([
-    React.PropTypes.number,
-    React.PropTypes.string
-  ]).isRequired
-};
-
 const handleClick = () => {
   fetch('http://localhost:8080/api/surveys', {
     method: 'POST',
@@ -54,15 +33,34 @@ const handleClick = () => {
       }]
     })
   })
-  .then(response =>
-    response.json()
-  )
+  .then(response => response.json())
   .then((result) => {
-    browserHistory.push(`survey/${result._id}/edit`);
+    browserHistory.push(`/survey/${result._id}/edit`);
   })
   .catch((error) => {
     throw error;
   });
+};
+
+const SurveyTile = props => (
+  <Col xs={12} sm={6} md={4}>
+    <Card style={styles.card}>
+      <CardTitle title={props.title} />
+      <CardActions>
+        <Link to={`/survey/${props.id}/edit`}><FlatButton label="Edit" /></Link>
+        <Link to={`/survey/${props.id}/results`}><FlatButton label="Results" /></Link>
+        <Link to={`/survey/${props.id}/answer`}><FlatButton label="Share" /></Link>
+      </CardActions>
+    </Card>
+  </Col>
+);
+
+SurveyTile.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  id: React.PropTypes.oneOfType([
+    React.PropTypes.number,
+    React.PropTypes.string
+  ]).isRequired
 };
 
 const SurveyGrid = props => (
