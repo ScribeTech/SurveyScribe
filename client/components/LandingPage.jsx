b1eda355fee052f79b4e44c40e3c3825d3c3479f
mrabuse <cogglemat@gmail.com>
2017-04-10 15:03:13 -0500

    Temp commit for rebasing

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Center from 'react-center';
 import { Link } from 'react-router';
 import RaisedButton from 'material-ui/RaisedButton';
 import { Grid, Row, Col } from 'react-flexbox-grid';
@@ -10,21 +10,17 @@ import { getSurveys } from '../utilities/getSurveys';
 
 const LandingPage = props => (
   <div>
-    <MuiThemeProvider>
-      <RaisedButton
-        href="https://github.com/ScribeTech/SurveyScribe"
-        label="Github Link"
-        icon={<img className="github" src={Github} alt="github" />}
-      />
-    </MuiThemeProvider>
+    <RaisedButton
+      href="https://github.com/ScribeTech/SurveyScribe"
+      label="Github Link"
+      icon={<img className="github" src={Github} alt="github" />}
+    />
     <Center >
       <img className="logo" src={Logo} alt="logo" />
     </Center>
-    <MuiThemeProvider>
-      <Center>
-        <RaisedButton label="Create Survey" style={{ margin: 12 }} onClick={() => getSurveys(props, 'survey')} />
-      </Center>
-    </MuiThemeProvider>
+    <Center>
+      <RaisedButton label="Create Survey" style={{ margin: 12 }} onClick={() => getSurveys(props, 'survey')} />
+    </Center>
     <footer >
       <p className="footer">Created by Nathan Clark Baumgartner, Megan Rabuse, Elijah Schow, and Jin Chung</p>
     </footer>
