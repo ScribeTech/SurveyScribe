e88413cbc5571fd1c77c6b49eb402a4185052baf
mrabuse <cogglemat@gmail.com>
2017-04-10 12:24:31 -0500

    Add sending ability to share

--- client/components/SurveyEdit.jsx
+++ client/components/SurveyEdit.jsx
@@ -15,10 +15,14 @@ import { getSurveys, updateSurvey } from '../utilities/getSurveys';
 const actions = (props, survey) => [
   { label: 'Save',
     callback: () => {
+      updateSurvey(props, survey, 'survey');
+    }
+  },
+  { label: 'Share',
+    callback: () => {
       updateSurvey(props, survey, `survey/${props.params.surveyID}/results`);
     }
   },
-  { label: 'Share', callback: () => {} },
   { label: 'Delete',
     callback: () => {
       fetch(`/api/surveys/${props.params.surveyID}`, {
