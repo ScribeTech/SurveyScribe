3f41d25b2aa0546be5bd7785939b628a89d4b0e4
Elijah Schow <elijah.schow@gmail.com>
2017-04-10 15:41:10 -0500

    Improve landing page styles

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
-import Center from 'react-center';
-import { Link } from 'react-router';
+
 import RaisedButton from 'material-ui/RaisedButton';
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import { Link } from 'react-router';
 
 import Logo from '../assets/Logo.svg';
 import Github from '../assets/GitHub-Mark.svg';
 import { getSurveys } from '../utilities/getSurveys';
 
 const LandingPage = props => (
-  <div>
-    <RaisedButton
-      href="https://github.com/ScribeTech/SurveyScribe"
-      label="Github Link"
-      icon={<img className="github" src={Github} alt="github" />}
-    />
-    <Center >
-      <img className="logo" src={Logo} alt="logo" />
-    </Center>
-    <Center>
-      <RaisedButton label="Create Survey" style={{ margin: 12 }} onClick={() => getSurveys(props, 'survey')} />
-    </Center>
-    <footer >
-      <p className="footer">Created by Nathan Clark Baumgartner, Megan Rabuse, Elijah Schow, and Jin Chung</p>
-    </footer>
+  <div className="landing-outer">
+    <Link to="https://github.com/ScribeTech/SurveyScribe">
+      <img className="github" src={Github} alt="Github" />
+    </Link>
+    <div className="landing-inner">
+      <div><img className="logo" src={Logo} alt="logo" /></div>
+      <div>
+        <RaisedButton label="Create Survey" style={{ margin: 12 }} onClick={() => getSurveys(props, 'survey')} />
+      </div>
+    </div>
+    <footer>Created by Nathan Clark Baumgartner, Megan Rabuse, Elijah Schow, and Jin Chung</footer>
   </div>
 );
 
