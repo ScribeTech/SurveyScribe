26b48cd72866e05547d5e6390b9a4a3939d3c0db
Elijah Schow <elijah.schow@gmail.com>
2017-04-10 13:02:26 -0500

    Optimize assets for deployment

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -1,47 +1,26 @@
 import React from 'react';
-import { Link, browserHistory } from 'react-router';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Center from 'react-center';
-import RaisedButton from 'material-ui/RaisedButton';
 
-import Logo from '../assets/Logo.png';
-import Github from '../assets/GitHub-Mark-32px.png';
+import { Link } from 'react-router';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
+import Logo from '../assets/Logo.svg';
+import Github from '../assets/GitHub-Mark.svg';
 import { getSurveys } from '../utilities/getSurveys';
 
 const LandingPage = props => (
   <div>
-    <MuiThemeProvider>
-      <RaisedButton
-        href="https://github.com/ScribeTech/SurveyScribe"
-        label="Github Link"
-        icon={<img className="github" src={Github} alt="github" />}
-      />
-    </MuiThemeProvider>
-    <Center >
-      <img className="logo" src={Logo} alt="logo" />
-    </Center>
-    <MuiThemeProvider>
-      <Center>
-        <RaisedButton label="Create Survey" style={{ margin: 12 }} onClick={() => getSurveys(props, 'survey')} />
-      </Center>
-    </MuiThemeProvider>
-
-    <Link to="/survey"> survey test link </Link>
-    <br />
-    <Link to="/survey/1/edit"> edit test link </Link>
-    <br />
-
-    <Link to="/survey/1/results"> results test link </Link>
-    <br />
-
-    <Link to="/survey/1/answer"> answer test link </Link>
-    <br />
-
-    <Link to="/survey/1/finish"> finish test link </Link>
-
-    <footer >
-      <p className="footer">Created by Nathan Clark Baumgartner, Megan Rabuse, Elijah Schow, Jin Chung</p>
-    </footer>
+    <img src={Logo} alt="logo" width="163px" height="109px" />
+    <img src={Github} alt="Github" width="32px" height="32px" />
+    <RaisedButton label="Create Survey" style={{ margin: 12 }} onClick={() => getSurveys(props, 'survey')} />
+    <ul>
+      <li><Link to="/survey"> survey test link</Link></li>
+      <li><Link to="/survey/1/edit"> edit test link</Link></li>
+      <li><Link to="/survey/1/results"> results test link</Link></li>
+      <li><Link to="/survey/1/answer"> answer test link</Link></li>
+      <li><Link to="/survey/1/finish"> finish test link</Link></li>
+    </ul>
+    <p className="footer">Created by Nathan Clark Baumgartner, Megan Rabuse, Elijah Schow, Jin Chung</p>
   </div>
 );
 
