fb5abce5b0b0c3384dfe7de210f35ce8bb5e7439
Won Jin Chung <chungw51993@gmail.com>
2017-04-11 11:36:19 -0500

    Fixing socket bug

--- server/socketio.js
+++ server/socketio.js
@@ -2,14 +2,16 @@ module.exports = function (io) {
   const clients = [];
   io.on('connection', (socket) => {
     clients.push(socket);
+    console.log(clients.length);
     socket.on('new vote', (data) => {
-      io.emit('change result', data);
+      socket.broadcast.emit('change result', data);
     });
-  });
 
-  io.on('disconnect', (socket) => {
-    const i = clients.indexOf(socket);
-    clients.splice(i, 1);
+    socket.on('disconnect', () => {
+      console.log(clients.length);
+      const i = clients.indexOf(socket);
+      clients.splice(i, 1);
+    });
   });
 };
 
