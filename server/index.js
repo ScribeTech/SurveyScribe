395774d53a4d568e619b3e78b32297861638c03f
Elijah Schow <elijah.schow@gmail.com>
2017-04-10 20:37:00 -0500

    (refactor) import style

--- server/index.js
+++ server/index.js
@@ -1,21 +1,16 @@
+const app = require('express')();
 const config = require('./config.js');
-
-const express = require('express');
-const bodyParser = require('body-parser');
 const SocketListener = require('./socketio.js');
-
-// Initialize the app
-const app = express();
 const db = require('./database.js');
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false })); // Parse data sent by clients
-app.use(bodyParser.json()); // Parse data sent by clients
+app.use(require('body-parser').urlencoded({ extended: false })); // Parse data sent by clients
+app.use(require('body-parser').json()); // Parse data sent by clients
 app.use(require('./middleware/session.js')); // track sessions
 app.use(require('./middleware/log.js')); // log activity
 
 // Routes
-app.use(express.static(config.public)); // server static files
+app.use(require('express').static(config.public)); // server static files
 app.use('/api', require('./controllers/api.js')); // handle api calls
 app.use(require('./middleware/error.js')); // handle errors
 
