0f4c985d70dcd71ddc26c6c23f8a8d42d27cb2f8
Won Jin Chung <chungw51993@gmail.com>
2017-04-13 14:19:15 -0500

    Routes test skeleton

--- tests/controller.authentication.test.js
+++ tests/controller.authentication.test.js
@@ -3,8 +3,12 @@ chai.use(require('chai-http'));
 chai.use(require('chai-shallow-deep-equal'));
 
 const { expect, request } = chai;
+<<<<<<< HEAD
 const app = require('../server/index.js');
 const { MethodNotAllowed } = require('./helpers/methodNotAllowed.js');
+=======
+const app = require('../index.js');
+>>>>>>> Routes test skeleton
 
 describe('Authentication routes', () => {
   describe('/api/login', () => {
@@ -18,11 +22,37 @@ describe('Authentication routes', () => {
       });
     });
 
+<<<<<<< HEAD
     describe('GET', MethodNotAllowed('get', '/api/login'));
 
     describe('PUT', MethodNotAllowed('put', '/api/login'));
 
     describe('DELETE', MethodNotAllowed('delete', '/api/login'));
+=======
+    describe('GET', () => {
+      it('should return 405 METHOD NOT ALLOWED', () => {
+
+      });
+    });
+
+    describe('PUT', () => {
+      it('should return 405 METHOD NOT ALLOWED', () => {
+
+      });
+    });
+
+    describe('PATCH', () => {
+      it('should return 405 METHOD NOT ALLOWED', () => {
+
+      });
+    });
+
+    describe('DELETE', () => {
+      it('should return 405 METHOD NOT ALLOWED', () => {
+
+      });
+    });
+>>>>>>> Routes test skeleton
   });
 
   describe('/api/logout', () => {
@@ -36,10 +66,36 @@ describe('Authentication routes', () => {
       });
     });
 
+<<<<<<< HEAD
     describe('GET', MethodNotAllowed('get', '/api/logout'));
 
     describe('PUT', MethodNotAllowed('put', '/api/logout'));
 
     describe('DELETE', MethodNotAllowed('delete', '/api/logout'));
+=======
+    describe('GET', () => {
+      it('should return 405 METHOD NOT ALLOWED', () => {
+
+      });
+    });
+
+    describe('PUT', () => {
+      it('should return 405 METHOD NOT ALLOWED', () => {
+
+      });
+    });
+
+    describe('PATCH', () => {
+      it('should return 405 METHOD NOT ALLOWED', () => {
+
+      });
+    });
+
+    describe('DELETE', () => {
+      it('should return 405 METHOD NOT ALLOWED', () => {
+
+      });
+    });
+>>>>>>> Routes test skeleton
   });
 });
