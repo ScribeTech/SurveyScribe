14758aa85c074aabd9a66e0af13e86f6842872f6
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-13 14:32:24 -0500

    got sign up paper up

--- client/components/SignIn.jsx
+++ client/components/SignIn.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Card, CardTitle, CardActions } from 'material-ui/Card';
 
-const SignIn = () => {
+const styles = {
+  main: {
+    position: 'relative',
+    top: 150,
+    margin: 'auto',
+    width: 600
+  }
+}
 
-};
+const SignIn = () => (
+  <div>
+    <Card style={styles.main}>
+      <CardTitle title="Please Sign Up" />
+      SignIn
+    </Card>
+  </div>
+);
 
-export default SignIn
+export default SignIn;
