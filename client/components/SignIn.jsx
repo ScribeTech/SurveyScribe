e43843624278a0b1218ff23ed788f5f6748af943
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-14 10:40:45 -0500

    create account button actually adding to mongogs

--- client/components/SignIn.jsx
+++ client/components/SignIn.jsx
@@ -24,27 +24,28 @@ const styles = {
 };
 
 const handleClick = (props) => {
-  console.log("nameVal", nameVal)
-  console.log("passWordVal", passWordVal)
-  // fetch('/api/users/:user', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({
-  //     title: 'New Survey',
-  //     questions: []
-  //   })
-  // })
-  // .then(response => response.json())
-  // .then((result) => {
-  //   // Adding survey to state and changing the view to edit
-  //   props.addSurvey(result._id, result.title);
-  //   browserHistory.push(`survey/${result._id}/${props.surveys.length}/edit`);
-  // })
-  // .catch((error) => {
-  //   throw error;
-  // });
+  console.log("nameVal", nameVal.input.value)
+  console.log("passWordVal", passWordVal.input.value)
+  fetch('/api/users/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: nameVal.input.value,
+      password: passWordVal.input.value
+    })
+  })
+  .then(response => response.json())
+  .then((result) => {
+    console.log("result", result)
+    // Adding survey to state and changing the view to edit
+    // props.addSurvey(result._id, result.title);
+    // browserHistory.push(`survey/${result._id}/${props.surveys.length}/edit`);
+  })
+  .catch((error) => {
+    throw error;
+  });
 };
 
 const SignIn = (props) => (
