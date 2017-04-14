49f6fd33e5e16cb23cbe82aac13278c82be502d7
Won Jin Chung <chungw51993@gmail.com>
2017-04-14 10:31:10 -0500

    Deleted test folder

---
+++ server/tests/controller.authentication.test.js
@@ -0,0 +1,45 @@
+const chai = require('chai');
+chai.use(require('chai-http'));
+chai.use(require('chai-shallow-deep-equal'));
+
+const { expect, request } = chai;
+const app = require('../index.js');
+const MethodNotAllowed = require('./helpers/methodNotAllowed.js');
+
+describe('Authentication routes', () => {
+  describe('/api/login', () => {
+    describe('POST', () => {
+      it('should return 200 and authenticate the user', () => {
+
+      });
+
+      it('should return 400 for invalid input', () => {
+
+      });
+    });
+
+    describe('GET', MethodNotAllowed('get', '/api/login'));
+
+    describe('PUT', MethodNotAllowed('put', '/api/login'));
+
+    describe('DELETE', MethodNotAllowed('delete', '/api/login'));
+  });
+
+  describe('/api/logout', () => {
+    describe('POST', () => {
+      it('should return 200 and all users', () => {
+
+      });
+
+      it('should return 401 if user\'s not authenticated', () => {
+
+      });
+    });
+
+    describe('GET', MethodNotAllowed('get', '/api/logout'));
+
+    describe('PUT', MethodNotAllowed('put', '/api/logout'));
+
+    describe('DELETE', MethodNotAllowed('delete', '/api/logout'));
+  });
+});
