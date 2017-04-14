f2a0bd09f7d0125c9c66c4ae3d590ab91070927a
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-13 19:20:55 -0500

    trying to use ref

--- client/components/SignIn.jsx
+++ client/components/SignIn.jsx
@@ -3,6 +3,10 @@ import { Card, CardTitle, CardActions } from 'material-ui/Card';
 import Layout from './Layout';
 import FlatButton from 'material-ui/FlatButton';
 import TextField from 'material-ui/TextField';
+import { browserHistory } from 'react-router';
+
+let nameVal = '';
+let passWordVal = '';
 
 const styles = {
   main: {
@@ -19,7 +23,31 @@ const styles = {
   }
 };
 
-const SignIn = () => (
+const handleClick = (props) => {
+  console.log("nameVal", nameVal)
+  console.log("passWordVal", passWordVal)
+  // fetch('/api/users/:user', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     title: 'New Survey',
+  //     questions: []
+  //   })
+  // })
+  // .then(response => response.json())
+  // .then((result) => {
+  //   // Adding survey to state and changing the view to edit
+  //   props.addSurvey(result._id, result.title);
+  //   browserHistory.push(`survey/${result._id}/${props.surveys.length}/edit`);
+  // })
+  // .catch((error) => {
+  //   throw error;
+  // });
+};
+
+const SignIn = (props) => (
   <Layout title="Sign In">
     <div>
       <Card style={styles.main}>
@@ -27,15 +55,17 @@ const SignIn = () => (
         <div style={styles.textbox}>
           <TextField
             floatingLabelText="Name"
+            ref={(name) => { nameVal = name; }}
           />
         </div>
         <div style={styles.textbox}>
           <TextField
             floatingLabelText="Password"
+            ref={(password) => { passWordVal = password; }}
           />
         </div>
         <CardActions>
-          <FlatButton style={styles.createaccount} label="Create Account" />
+          <FlatButton style={styles.createaccount} label="Create Account" onClick={() => handleClick(props)} />
         </CardActions>
       </Card>
     </div>
