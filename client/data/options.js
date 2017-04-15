be20d83bc2eaffbbb934dcb68ab107678fbf3b80
mrabuse <cogglemat@gmail.com>
2017-04-15 14:18:00 -0500

    Add edit_questions reducer and tests

--- client/data/options.js
+++ client/data/options.js
@@ -1,5 +1,4 @@
 export const options = {
-Add normalizeResponses and tests
   '58ee63c65a2d576d5125b4c1': [
     { "id": "58ee6466aa8ac36d6d74fe9a", "label": "Red"},
     { "id": "58ee6466aa8ac36d6d74fe9b", "label": "Green"},
