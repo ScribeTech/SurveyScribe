0f7db57ffb6f181d0a6d428c4ef4adb4c0f71dee
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-17 10:34:03 -0500

    user auth

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -25,6 +25,7 @@ exports.logout = (request, response) => {
 };
 
 exports.isLoggedIn = (request, response, next) => {
+  console.log('request', request);
   if (request.session.user) {
     next();
   } else {
