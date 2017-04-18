98b7e53eb37637fc8da6a93dfeec6de8a2468819
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-18 10:46:05 -0500

    slider graph printing

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -7,25 +7,82 @@ const Results = (props) => {
   // Load the currently selected survey
   // TODO: move this code to middleware (see issue #94)
   const surveyID = props.params.surveyID;
-  console.log("props.surveys", props.surveys)
-  console.log("surveyID", surveyID)
-  //const [survey] = props.surveys.filter(s => s.id === surveyID);
-  const survey = _.filter(props.survey, s => s.id === surveyID);
-  console.log("survey", survey);
+  const survey = _.filter(props.surveys, { id: surveyID });
 
-  const makeConfig = (question) => {
-    const votes = [];
-    const options = [];
-    props.options[question.id].forEach((option) => {
-      votes.push(option.votes);
-      options.push(option.label);
-    });
+  const histogram = (data, step) => {
+    const histo = {};
+    let x;
+    let i;
+    const arr = [];
+    // Group down
+    for (i = 0; i < data.length; i += 1) {
+      x = Math.floor(data[i][0] / step) * step;
+      if (!histo[x]) {
+        histo[x] = 0;
+      }
+      histo[x] += 1;
+    }
+
+    // Make the histo group into an array
+    for (x in histo) {
+      if (histo.hasOwnProperty((x))) {
+        arr.push([parseFloat(x), histo[x]]);
+      }
+    }
+
+    // Finally, sort the array
+    arr.sort((a, b) => a[0] - b[0]);
+
+    return arr;
+  };
+
+  const makeScaleConfig = (data, question) => {
+    // expects data to be 2d array
+    const graphData = [];
+    for (let i = 0; i < data.length; i += 1) {
+      graphData.push([data[i]]);
+    }
+    const config = {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: question.title
+      },
+      xAxis: {
+        gridLineWidth: 1
+      },
+      yAxis: [{
+        title: {
+          text: 'Histogram Count'
+        }
+      }, {
+        opposite: true,
+        title: {
+          text: 'Y value'
+        }
+      }],
+      series: [{
+        name: 'Histogram',
+        type: 'column',
+        data: histogram(graphData, 1),
+        pointPadding: 0,
+        groupPadding: 0,
+        pointPlacement: 'between'
+      }]
+    };
+
+    return config;
+  };
+
+  const makeSelectConfig = (data) => {
+    
     const config = {
       chart: {
         type: 'bar'
       },
       title: {
-        text: question.label
+        text: question.title
       },
       legend: {
         enabled: false
@@ -57,93 +114,84 @@ const Results = (props) => {
     return config;
   };
 
-  const graphData = [
-    [6.2], [6.5], [5.5], [5.0], [5.8],
-    [7.0], [5.1], [6.0], [7.2], [6.2],
-    [7.5], [7.9], [7.9], [5.4], [6.0],
-    [4.2], [6.2], [7.0], [5.0], [6.6],
-    [5.5], [7.0], [6.8], [7.5], [7.2],
-    [7.0], [7.0], [7.9], [7.5], [6.0],
-    [5.4], [6.0], [7.5], [6.0], [5.0]
-  ];
-
-
-  const histogram = (data, step) => {
-    const histo = {};
-    let x;
-    let i;
-    const arr = [];
-    // Group down
-    for (i = 0; i < data.length; i += 1) {
-      x = Math.floor(data[i][0] / step) * step;
-      if (!histo[x]) {
-        histo[x] = 0;
-      }
-      histo[x] += 1;
-    }
-
-    // Make the histo group into an array
-    for (x in histo) {
-      if (histo.hasOwnProperty((x))) {
-        arr.push([parseFloat(x), histo[x]]);
-      }
-    }
-
-    // Finally, sort the array
-    arr.sort((a, b) => a[0] - b[0]);
-
-    return arr;
-  };
-
-  const makeSliderConfig = (Data) => {
-    // const votes = [];
-    // const options = [];
-    // props.options[data.id].forEach((option) => {
+  const makeQuestionGraph = (question) => {
+    const votes = [];
+    const options = [];
+    const responses = [];
+    let config = '';
+    // props.options[question.id].forEach((option) => {
     //   votes.push(option.votes);
     //   options.push(option.label);
     // });
-    const config = {
-      chart: {
-        type: 'column'
-      },
-      title: {
-        text: 'Highcharts Histogram'
-      },
-      xAxis: {
-        gridLineWidth: 1
-      },
-      yAxis: [{
-        title: {
-          text: 'Histogram Count'
-        }
-      }, {
-        opposite: true,
-        title: {
-          text: 'Y value'
-        }
-      }],
-      series: [{
-        name: 'Histogram',
-        type: 'column',
-        data: histogram(graphData, 1),
-        pointPadding: 0,
-        groupPadding: 0,
-        pointPlacement: 'between'
-      }]
-    };
+    _.map(props.responses, (user) => {
+      _.map(user, (resQuestion) => {
+        const kind = props.questions[resQuestion.question].kind;
+        //console.log("resQuestion", resQuestion, kind);
+        if (kind === 'Scale') {
+          config = makeScaleConfig(props.aggregates[question.id], question);
+        } else if (kind === 'Text') {
 
+        } else if (kind === 'Select') {
+          config = makeSelectConfig(props.aggregates[question.id], question);
+        }
+      });
+    });
+    // const config = {
+    //   chart: {
+    //     type: 'bar'
+    //   },
+    //   title: {
+    //     text: question.title
+    //   },
+    //   legend: {
+    //     enabled: false
+    //   },
+    //   tooltip: {
+    //     valueSuffix: ' Votes',
+    //     followPointer: 'true',
+    //     pointFormat: '<b>{point.y}</b><br/>',
+    //   },
+    //   credits: {
+    //     enabled: false
+    //   },
+    //   xAxis: {
+    //     categories: options
+    //   },
+    //   plotOptions: {
+    //     series: {
+    //       animation: {
+    //         duration: 2000
+    //       }
+    //     }
+    //   },
+    //   series: [{
+    //     data: votes,
+    //     color: '#00bcd4'
+    //   }]
+    // };
+    console.log("config", config)
     return config;
   };
+
+  // const graphData = [
+  //   [6.2], [6.5], [5.5], [5.0], [5.8],
+  //   [7.0], [5.1], [6.0], [7.2], [6.2],
+  //   [7.5], [7.9], [7.9], [5.4], [6.0],
+  //   [4.2], [6.2], [7.0], [5.0], [6.6],
+  //   [5.5], [7.0], [6.8], [7.5], [7.2],
+  //   [7.0], [7.0], [7.9], [7.5], [6.0],
+  //   [5.4], [6.0], [7.5], [6.0], [5.0]
+  // ];
+
 
   return (
     <Layout title="Results">
       <h1>{survey.title}</h1>
       <div>
-        {_.map(props.questions[surveyID], question => (
-          <ReactHighcharts config={makeConfig(question)} />
+        {_.map(props.questions, question => (
+          <ReactHighcharts config={makeQuestionGraph(question)} />
         ))}
       </div>
-      <ReactHighcharts config={makeSliderConfig('data')} />
     </Layout>
   );
 };
