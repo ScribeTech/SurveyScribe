4718aab37d8f6d242b2bea7f94287b5fe6bef14c
Elijah Schow <elijah.schow@gmail.com>
2017-04-18 18:55:49 -0500

    (fix) React warning about no keys

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const authors = [
-  { name: 'Nathan Clark Baumgartner', url: 'https://github.com/Clarkbaum' },
-  { name: 'Megan Rabuse', url: 'https://github.com/mrabuse' },
-  { name: 'Elijah Schow', url: 'https://github.com/elijah-schow' },
-  { name: 'Jin Chung', url: 'https://github.com/chungw51993' },
+  { name: 'Nathan Clark Baumgartner', url: 'https://github.com/Clarkbaum', key: Math.random() },
+  { name: 'Megan Rabuse', url: 'https://github.com/mrabuse', key: Math.random() },
+  { name: 'Elijah Schow', url: 'https://github.com/elijah-schow', key: Math.random() },
+  { name: 'Jin Chung', url: 'https://github.com/chungw51993', key: Math.random() },
 ];
 
 const Footer = props => (
   <footer>
-    Created by {authors.map(({ name, url }, i) => (
-      <span>
+    Created by {authors.map(({ name, url, key }, i) => (
+      <span key={key}>
         {i ? ', ' : ''}
         <a href={url} rel="noopener noreferrer">
           {name}
