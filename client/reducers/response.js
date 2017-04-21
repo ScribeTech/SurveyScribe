4e9aa66772e7e5e4f642c7beca42b6776c1808b7
mrabuse <cogglemat@gmail.com>
2017-04-21 14:47:20 -0500

    Fix disabling bug

--- client/reducers/response.js
+++ client/reducers/response.js
@@ -43,8 +43,6 @@ export function response(state = {}, action) {
           return remove(state, action.questionId);
         case 'REMOVE_QUESTION':
           return remove(state, action.questionId);
-        case 'CLEAR_RESPONSE':
-          return {};
         default:
           return state;
       }
