1acb30af6529045a6254a77344fde4d08718d950
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-20 19:10:19 -0500

    recentered it

--- client/components/Finish.jsx
+++ client/components/Finish.jsx
@@ -11,15 +11,13 @@ const styles = {
 
 const Finish = () => (
   <Light>
-    <div style={styles.root}>
-      <div className="layout-semiwhole">
-        <h1>You made it!</h1>
-        {/* <div>
-          <Link to={`/survey/${props.params.surveyID}/results`}><RaisedButton label="Graph" /></Link>
-        </div> */}
-      </div>
-      <p> It is safe to close the page.</p>
+    <div style={styles.root} className="layout-semiwhole">
+      <h1>You made it!</h1>
+      {/* <div>
+        <Link to={`/survey/${props.params.surveyID}/results`}><RaisedButton label="Graph" /></Link>
+      </div> */}
     </div>
+    <p style={styles.root}>It is safe to close the page.</p>
   </Light>
 );
 
