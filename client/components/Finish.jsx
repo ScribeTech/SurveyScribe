85de6432ae5c46357dc8c96df57c7773f04a3db4
nathan clark baumgartner <nbaumg@hotmail.com>
2017-04-21 08:59:26 -0500

    finish component styling (#232)
    * recentered it
    
    * finish page styling

--- client/components/Finish.jsx
+++ client/components/Finish.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 
-import RaisedButton from 'material-ui/RaisedButton';
-import { Link } from 'react-router';
 import { Light } from './Theme';
 
 const styles = {
   root: {
-    display: 'flex',
-    'align-items': 'center',
-    'justify-content': 'center',
-    'text-align': 'center'
+    display: 'absolute',
+    textAlign: 'center'
   }
 };
 
-const Finish = props => (
+const Finish = () => (
   <Light>
-    <div className="layout-semiwhole">
-      <h1>You made it!</h1>
-      <p>It is safe to close the page.</p>
-      {/* <div>
-        <Link to={`/survey/${props.params.surveyID}/results`}><RaisedButton label="Graph" /></Link>
-      </div> */}
+    <div style={styles.root}>
+      <div className="layout-semiwhole">
+        <h1>You made it!</h1>
+        {/* <div>
+          <Link to={`/survey/${props.params.surveyID}/results`}><RaisedButton label="Graph" /></Link>
+        </div> */}
+      </div>
+      <p> It is safe to close the page.</p>
     </div>
   </Light>
 );
