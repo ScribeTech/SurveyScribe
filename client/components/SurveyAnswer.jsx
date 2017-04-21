920a995bb5625b4bbe95f8729179baef5acceab3
Jin Chung <chungw51993@gmail.com>
2017-04-21 14:22:00 -0500

    Should be able to go to answer page directly (#251)
    * Working on answer page
    
    * Working on getting answer page
    
    * Added class to the answer page
    
    * Removed normalize from router'
    
    git status
    '

--- client/components/SurveyAnswer.jsx
+++ client/components/SurveyAnswer.jsx
@@ -11,157 +11,170 @@ import { Light } from './Theme';
 import { postResponse } from '../utilities/apiTalk';
 import { normalizeSurvey } from '../utilities/normalize';
 
-const Answer = (props) => {
+class Answer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ''
+    };
+  }
   // Load the currently selected survey
   // TODO: move this code to middleware (see issue #94)
-  // const getSurvey = (currProps) => {
-  //   fetch(`/api/surveys/${currProps.params.surveyID}`, {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   })
-  //   .then(response => response.json())
-  //   .then((result) => {
-  //     console.log(result);
-  //     const converted = normalizeSurvey(result);
-  //     currProps.updateSurvey(converted.questions, converted.options);
-  //   })
-  //   .catch((error) => {
-  //     throw error;
-  //   });
-  // };
 
-  // getSurvey(props);
-
-  const disable = (questionId, selected, responses) => {
-    const question = props.questions[questionId];
-    const options = props.options[questionId];
-    if (question.maxSelection !== 0 && question.maxSelection === selected) {
-      Object.keys(options).forEach((oId) => {
-        if (!responses.value[oId]) {
-          props.toggleDisabled(question.id, oId, question.kind);
-        }
+  componentWillMount() {
+    fetch(`/api/surveys/${window.location.href.split('/')[4]}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then((result) => {
+      const converted = normalizeSurvey(result);
+      console.log(converted);
+      this.props.updateSurvey(converted.questions, converted.options);
+      this.setState({
+        title: result.title
       });
-    }
-  };
+    })
+    .catch((error) => {
+      throw error;
+    });
+  }
 
-  const action = [
-    <RaisedButton
-      label="Okay"
-      onTouchTap={() => props.hideWarning()}
-    />
-  ];
+  render() {
+    const disable = (questionId, selected, responses) => {
+      const question = this.props.questions[questionId];
+      const options = this.props.options[questionId];
+      if (question.maxSelection !== 0 && question.maxSelection === selected) {
+        Object.keys(options).forEach((oId) => {
+          if (!responses.value[oId]) {
+            this.props.toggleDisabled(question.id, oId, question.kind);
+          }
+        });
+      }
+    };
 
-  const toggleOptions = (questionId, id) => {
-    const options = props.options[questionId];
-    const option = options[id];
-    const question = props.questions[questionId];
-    const response = props.response[questionId];
+    const action = [
+      <RaisedButton
+        label="Okay"
+        onTouchTap={() => this.props.hideWarning()}
+      />
+    ];
 
-    props.toggleSelected(question.id, id, question.kind);
-    if (!option.selected) {
-      const responses = Object.assign({}, response,
-        { value: Object.assign({}, response.value, { [id]: id }) });
-      props.addAnswer(question.id, id, question.kind);
-      disable(question.id, question.selected + 1, responses);
-    } else {
-      props.removeAnswer(question.id, id, question.kind);
-      disable(question.id, question.selected, response);
-    }
-  };
+    const toggleOptions = (questionId, id) => {
+      const options = this.props.options[questionId];
+      const option = options[id];
+      const question = this.props.questions[questionId];
+      const response = this.props.response[questionId];
 
-  const renderOption = (questionId) => {
-    renderOption.propTypes = {}.isRequired;
-    const options = props.options[questionId];
-    return (<div>
-      {_.map(props.options[questionId], (option) => {
-        return (<ListItem
-          leftCheckbox={
-            <Checkbox
-              onCheck={() => {
-                toggleOptions(questionId, option.id);
-              }}
-              disabled={options[option.id].disabled}
-            />
-                       }
-          primaryText={options[option.id].label}
-        />);
-      })}
-    </div>);
-  };
+      this.props.toggleSelected(question.id, id, question.kind);
+      if (!option.selected) {
+        const responses = Object.assign({}, response,
+          { value: Object.assign({}, response.value, { [id]: id }) });
+        this.props.addAnswer(question.id, id, question.kind);
+        disable(question.id, question.selected + 1, responses);
+      } else {
+        this.props.removeAnswer(question.id, id, question.kind);
+        disable(question.id, question.selected, response);
+      }
+    };
 
-  const renderKind = (question) => {
-    const response = props.response[question.id];
-    switch (question.kind) {
-      case 'Select':
-        return (
+    const renderOption = (questionId) => {
+      renderOption.propTypes = {}.isRequired;
+      const options = this.props.options[questionId];
+      return (<div>
+        {_.map(this.props.options[questionId], option =>
+          <ListItem
+            leftCheckbox={
+              <Checkbox
+                onCheck={() => {
+                  toggleOptions(questionId, option.id);
+                }}
+                disabled={options[option.id].disabled}
+              />
+                         }
+            primaryText={options[option.id].label}
+          />
+        )}
+      </div>);
+    };
+
+    const renderKind = (question) => {
+      const response = this.props.response[question.id];
+      switch (question.kind) {
+        case 'Select':
+          return (
+            <div>
+              <h3>{`${question.title}`}</h3>
+              <List required={question.required}>
+                {renderOption(question.id)}
+              </List>
+            </div>
+          );
+        case 'Scale':
+          return (
+            <div>
+              <h3>{`${question.title}  ${response ? response.value
+                   : question.min}`}</h3>
+              <Slider
+                min={question.min}
+                max={question.max}
+                step={1}
+                required={question.required}
+                defaultValue={Math.floor(question.max / 2)}
+                value={response ? response.value
+                       : question.min}
+                onChange={(e, value) => {
+                  this.props.addAnswer(question.id, value, question.kind);
+                }}
+              />
+            </div>
+          );
+        case 'Text':
+          return (
+            <div>
+              <h3>{`${this.props.questions[question.id].title}`}</h3>
+              <Textfield
+                maxLength={this.props.questions[question.id].max}
+                required={this.props.questions[question.id].required}
+                rows={4}
+                fullWidth
+                multiLine
+                onChange={(e, value) => {
+                  this.props.addAnswer(question.id, value, question.kind);
+                }}
+              />
+            </div>
+          );
+        default:
+          break;
+      }
+      renderKind.propTypes = {}.isRequired;
+    };
+
+    // Render
+    return (
+      <Light>
+        <div className="layout-semiwhole">
+          <h1>{this.state.title}</h1>
           <div>
-            <h3>{`${question.title}`}</h3>
-            <List required={question.required}>
-              {renderOption(question.id)}
-            </List>
+            {_.map(this.props.questions, question => (
+              renderKind(question)
+            ))}
           </div>
-        );
-      case 'Scale':
-        return (
-          <div>
-            <h3>{`${question.title}  ${response ? response.value
-                 : question.min}`}</h3>
-            <Slider
-              min={question.min}
-              max={question.max}
-              step={1}
-              required={question.required}
-              defaultValue={Math.floor(question.max / 2)}
-              value={response ? response.value
-                     : question.min}
-              onChange={(e, value) => {
-                props.addAnswer(question.id, value, question.kind);
-              }}
-            />
-          </div>
-        );
-      case 'Text':
-        return (
-          <div>
-            <h3>{`${props.questions[question.id].title}`}</h3>
-            <Textfield
-              maxLength={props.questions[question.id].max}
-              required={props.questions[question.id].required}
-              rows={4}
-              fullWidth
-              multiLine
-              onChange={(e, value) => {
-                props.addAnswer(question.id, value, question.kind);
-              }}
-            />
-          </div>
-        );
-      default:
-        break;
-    }
-    renderKind.propTypes = {}.isRequired;
-  };
-
-  // Render
-  return (
-    <Light>
-      <div className="layout-semiwhole">
-        <h1>{props.surveys[props.params.surveyID].title}</h1>
-        <div>
-          {_.map(props.questions, question => (
-            renderKind(question)
-          ))}
+          <RaisedButton onClick={() => postResponse(this.props, `/survey/${this.props.params.surveyID}/finish`)} label="Submit Answers" primary fullWidth />
+          <Dialog
+            open={this.props.warning} actions={action}
+            onRequestClose={() => this.props.hideWarning()}
+          >
+          There is a required question you did not answer!
+          </Dialog>
         </div>
-        <RaisedButton onClick={() => postResponse(props, `/survey/${props.params.surveyID}/finish`)} label="Submit Answers" primary fullWidth />
-        <Dialog open={props.warning} actions={action} onRequestClose={() => props.hideWarning()}>
-        You didn't answer required question!
-        </Dialog>
-      </div>
-    </Light>
-  );
-};
+      </Light>
+    );
+  }
+}
 
 Answer.propTypes = {}.isRequired;
 
